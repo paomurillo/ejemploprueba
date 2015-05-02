02182ca004ffd03bebccbbc796f8db99e66b3f5e
Paola Murillo <paolizmurillo@yahoo.com>
2015-05-02 14:14:53 -0600

    ejercicio

---
+++ src/ejemplo_oswaldo/Ejemplo_Oswaldo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo_oswaldo;
+
+/**
+ *
+ * @author Jvargas
+ */
+public class Ejemplo_Oswaldo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ejemplo f = new Ejemplo();
+        f.setVisible(true);
+    }
+    
+}
